ef1b11c519d34368ef794f46eb161405cb42787a
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-01 09:52:48 -0800

    added CRUD routes

--- routes/fishes.js
+++ routes/fishes.js
@@ -10,3 +10,40 @@ router.get('/', async function(req, res, next) {
     return next(err);
   }
 });
+
+router.post('/', async function(req, res, next) {
+  try {
+    const result = await db.query(
+      'INSERT INTO fishes (name, type) VALUES ($1, $2) RETURNING *',
+      [req.body.name, req.body.type]
+    );
+    return res.json(result.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.patch('/:id', async function(req, res, next) {
+  try {
+    const result = await db.query(
+      'UPDATE fishes SET name=$1, type=$2 WHERE id=$3 RETURNING *',
+      [req.body.name, req.body.type, req.params.id]
+    );
+    return res.json(result.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.delete('/:id', async function(req, res, next) {
+  try {
+    const result = await db.query('DELETE FROM fishes WHERE id=$1', [
+      req.params.id
+    ]);
+    return res.json({ message: 'Deleted' });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
